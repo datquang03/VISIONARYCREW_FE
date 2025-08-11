6ffeec6260e1fed445b9f1f39e3722a8cae98cc1
datquang03 <datquang03@gmail.com>
2025-08-11 18:57:04 +0700

    add /api endpoint

--- src/services/httpMethods.js
+++ src/services/httpMethods.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "https://visionarycrew-be-rpo7.vercel.app";
+const API_URL = import.meta.env.VITE_API_URL || "https://visionarycrew-be-rpo7.vercel.app/api";
 
 // Create axios instance with default config
 const axiosInstance = axios.create({
